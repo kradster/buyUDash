44428d2bd42f51dae0945d139966131aa53ae855
vinod <vinodkaradiya@gmail.com>
2018-02-14 18:34:34 +0530

    change in rate.js

--- js/rates.js
+++ js/rates.js
@@ -1,44 +1,23 @@
-$('document').ready(function () {
+$('Document').ready(function () {
+
     var rateData = $("#ratesData");
     var myArray = [];
     var coins = [];
     var mycoins = [
-        ark = [],
-        bat = [],
-        bcc = [],
-        bts = [],
-        cloak = [],
-        cvc = [],
-        dash = [],
-        dcn = [],
-        dgb = [],
-        doge = [],
-        etc = [],
-        eth = [],
-        fct = [],
-        gnt = [],
-        inr = [],
-        lsk = [],
-        ltc = [],
-        neo = [],
-        nxt = [],
-        omg = [],
-        pay = [],
-        pivx = [],
-        qtum = [],
-        rep = [],
-        sc = [],
-        steem = [],
-        strat = [],
-        xem = [],
-        xmr = [],
-        xrp = [],
-        zec = []
-
-
+        [],[],[],[],[],[],[],[],[],[],
+        [],[],[],[],[],[],[],[],[],[],
+        [],[],[],[],[],[],[],[],[],[]
     ];
-    getRates();
-    //setInterval(getRates,1000);
+    var curruncy = [
+        'ark','bat','bcc','bts','cloak','cvc','dash','dgb','dcn','doge',
+        'etc','eth','fct','gnt','inr','lsk','ltc','neo','nxt','omg',
+        'pay','pivx','qtum','rep','sc','steem','xem','xmr','xrp','zec'
+    ];
+
+    setInterval(function(){
+        getRates();
+        console.log("okk");
+    },1000);
 
     function getRates() {
         $.ajax({
@@ -47,16 +26,26 @@ $('document').ready(function () {
             dataType: "json",
             data: {},
             success: function (data) {
+
                 for (var x in data) {
                     myArray.push(data[x]);
                 }
-                console.log(myArray);
                 for (var i in myArray[0].crypto) {
                     coins.push(myArray[0].crypto[i]);
                 }
-                console.log(coins);
+              //console.log("myarray "+myArray);
+              //console.log("mycoins "+mycoins);
                 extracData();
                 updataTable(mycoins);
+                mycoins = new Array();
+                coins = new Array();
+                myArray = new Array();
+
+
+
+                //console.log("myarray "+myArray);
+                //console.log("mycoins "+mycoins);
+
 
             },
             error: function (error) {
@@ -71,22 +60,41 @@ $('document').ready(function () {
         for (var i = 0; i < mycoins.length; i++) {
             mycoins[i].push(coins[i]);
         }
-        console.log(mycoins);
 
     }
 
     function updataTable(arr) {
         for (var i = 0; i < arr.length; i++) {
             var MyObj = arr[i][0];
-            var cname = MyObj.name;
+            var cname = curruncy[i];
             var ctrade = MyObj.last_trade;
             var cvol = MyObj.vol;
-            var c24low = MyObj.hr24_low;
-            var c24high = MyObj.hr24_high;
-            var change24 = MyObj.change24;
-            
-            rateData.append('<tr><td>' + (i) + '</td><td>' + (ctrade) + '</td><td>' + (cvol) + '</td><td>' + (c24low) + '</td><td>' + (c24high) + '</td><td>' + (change24) + '</td></tr>');
+            var c24low = MyObj.low_24;
+            var c24high = MyObj.high_24;
+            var change24 = MyObj.change;
+            var rnd = Math.floor((Math.random() * 30) + 1);
+
+        rateData.append('<tr><td>' + (cname) + '</td><td>' + (ctrade) + '</td><td>' + (cvol) + '</td><td>' + (c24low) + '</td><td>' + (c24high) + '</td><td>' + (change24) + '</td></tr>');
+
 
         }
+
+
     }
+
+
+//    if(typeof(EventSource) !== "undefined") {
+//    var source = new EventSource("http://localhost/buyUDash/ajax_rate.json");
+//    source.onmessage = function(event) {
+//        var jsn = JSON.parse(event.data.crypto);
+//        for(var i in jsn){
+//            id.push(jsn[i]);
+//            console.log(jsn[i]);
+//        }
+//        document.getElementById("ratesData").innerHTML = event.data + "<br>";
+//    };
+//} else {
+//    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+//}
+
 });
