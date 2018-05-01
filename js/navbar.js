32cdf52474531d0dd0366947f3dab94491ef4256
vinod <vinodkaradiya@gmail.com>
2018-05-01 17:45:03 +0530

    appification-> deposite,withdraw,bank popup added

--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,53 @@
-$(document).ready(function(){
-    var mynav = $("nav");
+$(document).ready(function () {
+    var mynav = $("mynav");
+    var mynav2 = $("mywalletnav");
     var tb = $('#total-balance');
     $('#ribbon').hide();
-    $(window).scroll(function(){
-        var hight = $(this).scrollTop();
 
-        console.log(hight);
-        if(hight>0){
+
+
+    $(window).scroll(function () {
+        var hight = $(this).scrollTop();
+        //console.log('hi');
+        //console.log(hight);
+
+
+        
+        if (hight > 0) {
             $(".nav-content").hide(300);
             $("#anim_row").hide(300);
             $('#ribbon').hide();
-            tb.removeClass('hide');
-        }
-        else{
+            tb.css({"left":"0px","opacity":"1"});
+            
+            
+            
+        } else {
             $(".nav-content").show(300);
             $("#anim_row").show(300);
-            tb.addClass('hide');
+            tb.css({"left":"-100px","opacity":"0"});
 
         }
-        if(hight>471){
+        
+
+        if (hight > 471) {
             //mynav.addClass("blue darken-4");
-            mynav.css("background","linear-gradient(-90deg,rgb(46,49,146) 0%,rgb(41,171,226) 100%)");
+            mynav.css("background", "linear-gradient(-90deg,rgb(46,49,146) 0%,rgb(41,171,226) 100%)");
+            mynav2.css("background", "linear-gradient(-90deg,rgb(46,49,146) 0%,rgb(41,171,226) 100%)");
 
-        }
-        else{
-            
+        } else {
+
             //mynav.removeClass("blue darken-4");
-            mynav.css("background","transparent");
+            mynav.css("background", "transparent");
+            mynav2.css("background", "linear-gradient(-90deg,rgb(46,49,146) 0%,rgb(41,171,226) 100%)");
         }
-        if(hight>665){
+        if (hight > 665) {
             $('#ribbon').show();
         }
-        if(hight<665){
+        if (hight < 665) {
             $('#ribbon').hide();
         }
+        
+
+
     });
 });
